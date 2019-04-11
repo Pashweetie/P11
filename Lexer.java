44f1f6d171aed534349f7ff71db4d2bc5f93c39e
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-04-11 12:36:27 -0600

    first commit

---
+++ Lexer.java
@@ -0,0 +1,287 @@
+import java.util.*;
+import java.io.*;
+public class Lexer {
+
+    public static String margin = "";
+
+    // holds any number of tokens that have been put back
+    private Stack<Token> stack;
+
+    // the source of physical symbols
+    // (use BufferedReader instead of Scanner because it can
+    //  read a single physical symbol)
+    private BufferedReader input;
+
+    // one lookahead physical symbol
+    private int lookahead;
+
+    // construct a Lexer ready to produce tokens from a file
+    public Lexer( String fileName ) {
+        try {
+            input = new BufferedReader( new FileReader( fileName ) );
+        }
+        catch(Exception e) {
+            error("Problem opening file named [" + fileName + "]" );
+        }
+        stack = new Stack<Token>();
+        lookahead = 0;  // indicates no lookahead symbol present
+    }// constructor
+
+    // produce the next token
+    private Token getNext() {
+        if( ! stack.empty() ) {
+            //  produce the most recently putback token
+            Token token = stack.pop();
+            return token;
+        }
+        else {
+            // produce a token from the input source
+
+            int state = 0;  // state of FA
+            String data = "";  // specific info for the token
+            boolean done = false;
+            int sym;  // holds current symbol
+
+            do {
+                sym = getNextSymbol();
+
+// System.out.println("current symbol: " + sym + " state = " + state );
+
+                if ( state == 0 ) {
+                    if ( sym == 9 || sym == 10 || sym == 13 ||
+                            sym == 32 ) {// whitespace
+                        state = 0;
+                    }
+                    else if ( letter(sym) ) {// any letter (not just lowercase)
+                        data += (char) sym;
+                        state = 1;
+                    }
+                    else if ( digit( sym ) ) {
+                        data += (char) sym;
+                        state = 4;
+                    }
+                    else if ( sym == '-'){
+                        state = 2;
+                    }
+                    else if ( sym == ';' ) {
+                        data += (char) sym;
+                        state = 5;
+                    }
+                    else if (sym == '(' || sym == ')') {
+                        data += (char) sym;
+                        state = 8;
+                        done = true;
+                    }
+                    else if ( sym == -1 ) {// end of file
+                        state = 7;
+                        done = true;
+                    }
+                    else {
+                        error("Error in lexical analysis phase with symbol "
+                                + sym + " in state " + state );
+                    }
+                }
+                else if ( state == 1){
+                    if( letter(sym)){
+                        data += (char) sym;
+                        state = 1;
+                    }
+                    else {
+                        putBackSymbol(sym);
+                        done =true;
+                    }
+                }
+                else if ( state == 2 ) {
+                    if (digit(sym) ) {
+                        data += (char) sym;
+                        state = 3;
+                    }
+                    else {// done with variable token
+                        error("a minus sign must be followed by a number");
+                    }
+                }
+
+                else if ( state == 3 ) {
+                    if ( digit(sym) ) {
+                        data += (char) sym;
+                        state = 3;
+                    }
+                    else if ( sym == '.' ) {
+                        data += (char) sym;
+                        state = 9;
+                    }
+                    else {// done with number token
+                        putBackSymbol( sym );
+                        done = true;
+                    }
+
+                }
+
+                else if ( state == 4 ) {
+                    if ( digit(sym) ) {
+                        data += (char) sym;
+                        state = 4;
+                    }
+                    else if (sym=='.'){
+                        data += (char)sym;
+                        state = 10;
+                    }
+                    else {// done with number token
+                        putBackSymbol( sym );
+                        done = true;
+                    }
+                }
+
+                else if ( state == 5 ) {
+                    if ( sym==13||sym==10 ) {
+                        done = true;
+                    }
+                    else {
+                        state = 5;
+                    }
+                }
+
+                else if ( state == 6 ) {
+                    if ( (' '<=sym && sym<='~') && sym != '\"' ) {
+                        data += (char) sym;
+                        state = 6;
+                    }
+                    else if ( sym == '\"' ) {
+                        state = 7;
+                        done = true;
+                    }
+                }
+
+
+
+
+            }while( !done );
+
+            // generate token depending on stopping state
+            Token token;
+            if ( state == 1){
+                if ( data.equals("ins") || data.equals("first") ||
+                        data.equals("rest") || data.equals("null") ||
+                        data.equals("num") || data.equals("list") ||
+                        data.equals("read") ||data.equals("write") ||
+                        data.equals("nl") ||data.equals("quote") ||
+                        data.equals("quit") ||data.equals("plus") ||
+                        data.equals("minus") ||data.equals("times") ||
+                        data.equals("div") ||data.equals("lt") ||
+                        data.equals("le") ||data.equals("eq") ||
+                        data.equals("ne") ||data.equals("and") ||
+                        data.equals("or") ||data.equals("not") ||
+                        data.equals("define")
+                        ) {
+                    return new Token( "KEYWORD", data );
+                }
+                else{
+                    return new Token ("NAME",data);
+                }
+            }
+            if(state == 3){
+                return new Token("NEGATIVE NUMBER",data);
+            }
+            if(state == 4){
+                return new Token("NUMBER",data);
+            }
+            if ( state == 5 ) {
+                return new Token("COMMENT",data);
+            }
+            else if ( state==9 ) {
+                return new Token( "NEGATIVE DECIMAL NUMBER", data );
+            }
+            else if ( state == 10 ) {
+                return new Token( "DECIMAL NUMBER", data );
+            }
+            else if ( state == 8 ) {
+                if(data.equals("(")){
+                    return new Token ("LPAREN", data);
+                } else if(data.equals(")")){
+                    return new Token ("RPAREN", data);
+                }
+            }
+            else if ( state == 7 ) {
+                return new Token( "eof", data );
+            }else {// Lexer error
+                error("somehow Lexer FA halted in bad state " + state );
+                return null;
+            }
+
+        }// else generate token from input
+        return null;
+    }// getNext
+
+    public Token getNextToken() {
+        Token token = getNext();
+        System.out.println("                     got token: " + token );
+        return token;
+    }
+
+    public void putBackToken( Token token )
+    {
+        System.out.println( margin + "put back token " + token.toString() );
+        stack.push( token );
+    }
+
+    // next physical symbol is the lookahead symbol if there is one,
+    // otherwise is next symbol from file
+    private int getNextSymbol() {
+        int result = -1;
+
+        if( lookahead == 0 ) {// is no lookahead, use input
+            try{  result = input.read();  }
+            catch(Exception e){}
+        }
+        else {// use the lookahead and consume it
+            result = lookahead;
+            lookahead = 0;
+        }
+        return result;
+    }
+
+    private void putBackSymbol( int sym ) {
+        if( lookahead == 0 ) {// sensible to put one back
+            lookahead = sym;
+        }
+        else {
+            System.out.println("Oops, already have a lookahead " + lookahead +
+                    " when trying to put back symbol " + sym );
+            System.exit(1);
+        }
+    }// putBackSymbol
+
+    private boolean letter( int code ) {
+        return 'a'<=code && code<='z' ||
+                'A'<=code && code<='Z';
+    }
+
+    private boolean digit( int code ) {
+        return '0'<=code && code<='9';
+    }
+
+    private boolean printable( int code ) {
+        return ' '<=code && code<='~';
+    }
+
+    private static void error( String message ) {
+        System.out.println( message );
+        System.exit(1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("Enter file name: ");
+        Scanner keys = new Scanner( System.in );
+        String name = keys.nextLine();
+
+        Lexer lex = new Lexer( name );
+        Token token;
+
+        do{
+            token = lex.getNext();
+            System.out.println( token.toString() );
+        }while( ! token.getKind().equals( "eof" )  );
+
+    }
+
+}
---
+++ Parser.java
@@ -0,0 +1,152 @@
+public class Parser {
+
+    private Lexer lex;
+
+    public Parser( Lexer lexer ) {
+        lex = lexer;
+    }
+
+    public Node parseDefs() {
+        System.out.println("-----> parsing <defs>:");
+        Node first = parseDef();
+        Token token = lex.getNextToken();
+        if ( token.isKind("eof") ){
+            return new Node("defs", first, null, null);
+        }
+        else {
+            lex.putBackToken(token);
+            Node second = parseDefs();
+            return new Node("defs", first, second, null);
+        }
+    }
+    public Node parseDef(){
+        System.out.println("-----> parsing <def>:");
+
+
+        Token token = lex.getNextToken();
+        errorCheck(token, "LPAREN", "(");
+
+        token = lex.getNextToken();
+        errorCheck(token, "KEYWORD");
+
+        token = lex.getNextToken();
+        errorCheck(token, "LPAREN", "(");
+
+        Token name = lex.getNextToken();
+        errorCheck(name, "NAME");
+
+        token = lex.getNextToken();
+
+        if ( token.isKind("RPAREN") ) {
+            Node first = parseExpr();
+            token = lex.getNextToken();
+            errorCheck(token, "RPAREN", ")");
+            return new Node("def", name.getDetails(),
+                    first, null, null);
+        }
+        else {
+            lex.putBackToken( token );
+            Node first = parseParams();
+            token = lex.getNextToken();
+            errorCheck(token, "RPAREN", ")");
+            Node second = parseExpr();
+            token = lex.getNextToken();
+            errorCheck(token, "RPAREN", ")");
+            return new Node("def", name.getDetails(),
+                    first, second, null);
+        }
+    }
+    public Node parseParams() {
+        System.out.println("-----> parsing <params>:");
+
+        Token name = lex.getNextToken();
+        errorCheck(name, "NAME");
+
+        Token token = lex.getNextToken();
+
+        if ( token.isKind("RPAREN") ) {
+            lex.putBackToken( token );
+            return new Node("params", name.getDetails(),
+                    null, null, null);
+        }
+        else{
+            lex.putBackToken( token );
+            Node first = parseParams();
+            return new Node("params", name.getDetails(),
+                    first, null, null);
+        }
+    }
+
+    public Node parseExpr() {
+        System.out.println("-----> parsing <expr>:");
+
+        Token token = lex.getNextToken();
+
+        if ( token.isKind("LPAREN") ) {
+            lex.putBackToken( token );
+            Node first = parseList();
+            return new Node("expr", first, null, null);
+        }
+        else{
+            return new Node("expr", token.getDetails(),
+                    null, null, null);
+        }
+    }
+
+    public Node parseList(){
+        System.out.println("-----> parsing <list>:");
+
+        Token token = lex.getNextToken();
+        errorCheck(token, "LPAREN", "(");
+
+        token = lex.getNextToken();
+
+        if ( token.isKind("RPAREN") ) {
+            return new Node("list", null, null, null);
+        }
+        else {
+            lex.putBackToken( token );
+            Node first = parseItems();
+            token = lex.getNextToken();
+            errorCheck(token, "RPAREN", ")");
+            return new Node("list", first, null, null);
+        }
+    }
+    public Node parseItems(){
+        System.out.println("-----> parsing <items>:");
+
+        Node first = parseExpr();
+        Token token = lex.getNextToken();
+
+        if ( token.isKind("RPAREN") ) {
+            lex.putBackToken( token );
+            return new Node("items", first, null, null);
+        }
+        else {
+            lex.putBackToken( token );
+            Node second = parseItems();
+            return new Node("items", first, second, null);
+        }
+    }
+
+    // check whether token is correct kind
+    private void errorCheck( Token token, String kind ) {
+        if( ! token.isKind( kind ) ) {
+            System.out.println("Error:  expected " + token +
+                    " to be of kind " + kind );
+            System.exit(1);
+        }
+    }
+
+    // check whether token is correct kind and details
+    private void errorCheck( Token token, String kind, String details ) {
+        if( ! token.isKind( kind ) ||
+                ! token.getDetails().equals( details ) ) {
+            System.out.println("Error:  expected " + token +
+                    " to be kind= " + kind +
+                    " and details= " + details );
+            System.exit(1);
+        }
+    }
+
+}
