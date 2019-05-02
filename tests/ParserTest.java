e96b85fa4fe0be7e7b58bb5c6a84b5ba0bc8f6e3
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-05-02 12:22:32 -0600

    Create ParserTest.java

---
+++ tests/ParserTest.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void parseProgram() {
+
+    }
+
+    @Test
+    public void parseName() {
+    }
+
+    @Test
+    public void parseDef() {
+    }
+}
